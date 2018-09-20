b1fd13ba1dfa0c36b2eaf0a2ad5734deb8c9953f
Reece_Collett <rcollett@spartaglobal.com>
2018-09-20 12:23:22 +0100

    Edited provision file

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -21,9 +21,8 @@ sudo apt-get install nodejs -y
 sudo npm install pm2 -g
 
 # Remove default nginx site
-sudo rm /etc/nginx/sites-enabled/default
 sudo rm /etc/nginx/sites-available/default
 
 # set up nginx reverse proxy
-sudo ln -s /home/ubuntu/environment/app/nodeapp.conf /etc/nginx/conf.d/nodeapp.conf
+sudo ln -s /home/ubuntu/environment/app/default /etc/nginx/sites-available/default
 sudo nginx -s reload
