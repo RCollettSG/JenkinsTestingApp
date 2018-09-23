30eda0dc0c13491ef391f1af327219c2458bd4a1
Reece_Collett <rcollett@spartaglobal.com>
2018-09-23 13:46:29 +0100

    Added to the provision to create a /data/db folder for mongo

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -10,5 +10,7 @@ sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org
 sudo rm /etc/mongod.conf
 sudo ln -s /home/ubuntu/environment/db/mongod.conf /etc/mongod.conf
 
+sudo mkdir -p /data/db
+
 sudo systemctl restart mongod
 sudo systemctl enable mongod
