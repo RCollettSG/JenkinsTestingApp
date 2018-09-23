df7200acca19e77bd1968f3f96ab7ea46335c5a0
Reece_Collett <rcollett@spartaglobal.com>
2018-09-23 12:52:46 +0100

    Changed provision file

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -8,7 +8,7 @@ sudo apt-get upgrade -y
 sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
 
 sudo rm /etc/mongod.conf
-sudo ln -s /home/ubuntu/environment/mongod.conf /etc/mongod.conf
+sudo ln -s /home/ubuntu/environment/db/mongod.conf /etc/mongod.conf
 
 sudo systemctl restart mongod
 sudo systemctl enable mongod
